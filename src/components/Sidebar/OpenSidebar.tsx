4adea330a341b21b1ade6fc31bc4dc971bfa2c6d
rafaapcode <rafaap2003@gmail.com>
2024-12-22 17:18:41 -0300

    feat: creating the pages

--- src/components/Sidebar/OpenSidebar.tsx
+++ src/components/Sidebar/OpenSidebar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { BsGrid } from "react-icons/bs";
 import { GoHomeFill } from "react-icons/go";
 import { IoIosSettings } from "react-icons/io";
@@ -7,16 +8,18 @@ import UserImg from "../../../public/user-img.jpg";
 import MenuItems from "./MenuItems";
 
 function OpenSidebar() {
+  const pathName = usePathname();
+    
   return (
     <section className="bg-[#F5F7F9] overflow-hidden flex flex-col p-3 w-[10%] transition-all duration-300">
       <header className="mb-5">
         Finance App Logo
       </header>
       <section className="flex-1 flex flex-col gap-5">
-        <MenuItems Icon={GoHomeFill} isActive title="Home"/>
-        <MenuItems routeName="investments" Icon={PiMoneyWavyFill} isActive={false} title="Investimentos"/>
-        <MenuItems routeName="settings" Icon={IoIosSettings} isActive={false} title="Configurações"/>
-        <MenuItems routeName="general" Icon={BsGrid} isActive={false} title="Visão Geral"/>
+        <MenuItems Icon={GoHomeFill} isActive={pathName === "/main/home"} title="Home"/>
+        <MenuItems routeName="investments" Icon={PiMoneyWavyFill} isActive={pathName === "/main/investments"} title="Investimentos"/>
+        <MenuItems routeName="settings" Icon={IoIosSettings} isActive={pathName === "/main/settings"} title="Configurações"/>
+        <MenuItems routeName="general" Icon={BsGrid} isActive={pathName === "/main/general"} title="Visão Geral"/>
       </section>
       <div className="mb-10">
           <p>Total</p>
