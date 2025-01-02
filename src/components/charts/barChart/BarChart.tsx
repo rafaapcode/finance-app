43b3d6c8d35ab36c702524e6a1de1c2c4422a821
rafaapcode <rafaap2003@gmail.com>
2025-01-02 20:12:53 -0300

    feat: creating the general page

---
+++ src/components/charts/barChart/BarChart.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig
+
+function BarChartComponent() {
+  return (
+    <Card className="h-full">
+      <CardHeader>
+        <CardTitle>Bar Chart</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent className="h-full">
+        <ChartContainer config={chartConfig} className="h-[80%] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BarChartComponent;
--- src/pages/General/page.tsx
+++ src/pages/General/page.tsx
@@ -1,8 +1,21 @@
+import BarChartComponent from "@/components/charts/barChart/BarChart";
+import { PieChartComponent } from "@/components/charts/pieChart/PieChart";
+
 function GeneralPage() {
   return (
-    <div>
-      <h1>GeneralPage</h1>
-    </div>
+    <main className="h-full flex-1 overflow-y-auto scrollbar-none">
+      <div className="w-full h-1/2 max-h-[430px]">
+        <BarChartComponent />
+      </div>
+      <div className="flex gap-4 mt-2 h-1/2">
+        <div className="w-1/2 h-full">
+          <PieChartComponent />
+        </div>
+        <div className="w-1/2 h-full">
+          <BarChartComponent />
+        </div>
+      </div>
+    </main>
   );
 }
 
